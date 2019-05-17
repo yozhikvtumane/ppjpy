75d20951da68a0ac52375b58ced5e3fa700fc4e2
yozhikvtumane <toshamatveeev@gmail.com>
2019-05-17 10:33:47 +0200

    upd

--- dice.py
+++ dice.py
@@ -1,3 +1,33 @@
+# import numpy as np
+# import numpy.random as rnd
+# import math
+
+# res = {}
+
+# # throw = rnd.randint(1, 6+1, 5)
+# throw = np.array([2,2,3,3,2])
+# print(throw)
+# for i in range (1,7):
+#     print(np.count_nonzero(throw == i))
+#     res[i] = np.count_nonzero(throw == i)
+#     # if res[i]==3:
+#     #     if
+
+# for v,k in res.items():
+#     if k == 2:
+#         res1 = "yes"
+#         print('v=',v)
+
+# if res1:
+#     print(res1)
+
+
+# print(res.values())
+# throw.sort()
+# print(throw)
+# print(res)
+
+
 import numpy as np
 import numpy.random as rnd
 import math
@@ -14,8 +44,8 @@ combs = {
 }
 
 # throw = rnd.randint(1, 6+1, 5)
-throw = np.array([1,1,1,1,1])
-print(throw)
+throw = np.array([2,2,2,3,3])
+print("Your current throw of five dices is %s" %throw)
 for i in range (1,7):
     print(np.count_nonzero(throw == i))
     res[i] = np.count_nonzero(throw == i)
@@ -25,6 +55,12 @@ for i in res.keys():
         print("%s : %s %s + %s" % (combs[str(i)], i * res[i] , [z for z in throw if z == i] , [z for z in throw if z != i]))
     if res[i] == 3:
         print(("Three of a kind : %s %s + %s" % (np.sum(throw) , [z for z in throw if z == i] , [z for z in throw if z != i])))
+        
+        for v,k in res.items():
+            if k == 2:
+                arr1 = [z for z in throw if z == i]
+                arr2 = [z for z in throw if z == v]
+                print("Fullhouse! : 25  %s + []" % (arr1 + arr2))
     if res[i] == 4:
         print(("Four of a kind : %s %s + %s" % (np.sum(throw) , [z for z in throw if z == i] , [z for z in throw if z != i])))
     if res[i] == 5:
